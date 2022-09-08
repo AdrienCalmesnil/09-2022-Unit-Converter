a21d560759498dec685aaebf927b9f68d89c646a
Adrien Calmesnil <adcalmesnil@gmail.com>
2022-09-08 23:01:26 +0200

    dark mode activated

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let numberEl = document.getElementById('number-el')
 const lengthEl = document.getElementById('length-el')
 const volumeEl = document.getElementById('volume-el')
 const massEl = document.getElementById('mass-el')
+const darkBtn = document.getElementById('dark-btn')
 
 
 convertBtn.addEventListener('click', function(){
@@ -18,4 +19,35 @@ convertBtn.addEventListener('click', function(){
     volumeEl.textContent = volumeString
     let massString = `${numberEl.value} kilos = ${(numberEl.value * 2.204).toFixed(3)} pounds | ${numberEl.value} pounds = ${(numberEl.value / 2.204).toFixed(3)} kilos`
     massEl.textContent = massString
+})
+
+
+
+// DARK MODE FUNCTION
+let x = document.getElementsByClassName('box-switch')
+let y = document.getElementsByClassName('title-switch')
+let z = document.getElementsByClassName('p-switch')
+
+darkBtn.addEventListener('click', function(){
+    if(document.body.style.background === "white"){
+        document.body.style.background = "radial-gradient(circle, rgba(105,67,255,1) 0%, rgba(34,32,41,1) 61%, rgba(34,32,41,1) 100%)"
+        document.getElementById('converters-switch').style.backgroundColor = "#1F2937"
+
+        for(let i = 0; i < x.length; i++){
+            x[i].style.backgroundColor = "#273549"
+            y[i].style.color = "#CCC1FF"
+            z[i].style.color = "#fff"
+        }
+        
+    }else{
+        document.body.style.background = "white"
+        document.getElementById('converters-switch').style.backgroundColor = "#f4f4f4"
+        
+        for(let i = 0; i < x.length; i++){
+            x[i].style.backgroundColor = "#fff"
+            y[i].style.color = "#5A537B"
+            z[i].style.color = "#353535"
+        }
+
+    }
 })
